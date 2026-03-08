18b355cd0805e88c25ea7b98fbf43307b2d5cb27
ntalbs <ntalbsen@gmail.com>
2026-03-08 17:42:48 +0000

    Change TokenType to Token with Pos

--- src/token.rs
+++ src/token.rs
@@ -1,15 +1,59 @@
-pub(crate) enum TokenType {
-  LeftParen, RightParen, LeftBrace, RightBrace,
-  Comma, Dot, Minus, Plus, Semicolon, Slash, Star,
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub(crate) struct Pos {
+  pub line: usize,
+  pub col: usize,
+}
 
-  Bang, BangEqual,
-  Equal, EqualEqual,
-  Greater, GreaterEqual,
-  Less, LessEqual,
+pub(crate) enum Token {
+    LeftParen(Pos),
+    RightParen(Pos),
+    LeftBrace(Pos),
+    RightBrace(Pos),
+    Comma(Pos),
+    Dot(Pos),
+    Minus(Pos),
+    Plus(Pos),
+    Semicolon(Pos),
+    Slash(Pos),
+    Star(Pos),
 
-  Identifier, String, Number,
-  And, Class, Else, False, Fun, For, If, Nil, Or,
-  Print, Return, Super, This, True, Var, While,
+    Bang(Pos),
+    BangEqual(Pos),
+    Equal(Pos),
+    EqualEqual(Pos),
+    Greater(Pos),
+    GreaterEqual(Pos),
+    Less(Pos),
+    LessEqual(Pos),
 
-  Eof,
+    Identifier {
+      lexeme: String,
+      pos: Pos,
+    },
+    String {
+      lexeme: String,
+      pos: Pos,
+    },
+    Number {
+      lexeme: String,
+      pos: Pos,
+    },
+    And(Pos),
+    Class(Pos),
+    Else(Pos),
+    False(Pos),
+    Fun(Pos),
+    For(Pos),
+    If(Pos),
+    Nil(Pos),
+    Or(Pos),
+    Print(Pos),
+    Return(Pos),
+    Super(Pos),
+    This(Pos),
+    True(Pos),
+    Var(Pos),
+    While(Pos),
+
+    Eof,
 }
